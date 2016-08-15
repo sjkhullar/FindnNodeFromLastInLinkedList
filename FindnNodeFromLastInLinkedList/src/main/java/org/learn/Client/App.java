0096cc4eb76060b4481422c842892091e2e5e972
makeinjava <makeinjava@gmail.com>
2016-08-15 23:27:21 +0530

    updated code

--- FindnNodeFromLastInLinkedList/src/main/java/org/learn/Client/App.java
+++ FindnNodeFromLastInLinkedList/src/main/java/org/learn/Client/App.java
@@ -3,30 +3,31 @@ package org.learn.Client;
 import org.learn.List.Node;
 import org.learn.Question.FindNthNodeFromLast;
 
-public class App 
-{
-	public static void main( String[] args )
-    {
-		int[] data = {10, 20, 30, 40, 50};
+public class App {
+	public static void main(String[] args) {
+		int[] data = { 10, 20, 30, 40, 50 };
 		Node head = new Node(data[0]);
-		for(int count = 1; count < data.length; count++)
-			FindNthNodeFromLast.insert(head,data[count]);
-		System.out.printf("Linked list is : ");
+		for (int count = 1; count < data.length; count++)
+			FindNthNodeFromLast.insert(head, data[count]);
+		System.out.printf("1. Single linked list is : ");
 		FindNthNodeFromLast.print(head);
+
 		int n = 1;
-		Node nodeFromLast = FindNthNodeFromLast.findNthNodeFromLast(head,n);
-		if(null != nodeFromLast) {
-			System.out.printf("Node from last for n = %d is %d",n,nodeFromLast.data);
+		Node nodeFromLast = FindNthNodeFromLast.findNthNodeFromLast(head, n);
+		if (null != nodeFromLast) {
+			System.out.printf("2. Node from last for n = %d is %d", n, nodeFromLast.data);
 		}
+
 		n = 3;
-		nodeFromLast = FindNthNodeFromLast.findNthNodeFromLast(head,n);
-		if(null != nodeFromLast) {
-			System.out.printf("\nNode from last for n = %d is %d",n,nodeFromLast.data);
+		nodeFromLast = FindNthNodeFromLast.findNthNodeFromLast(head, n);
+		if (null != nodeFromLast) {
+			System.out.printf("\n3. Node from last for n = %d is %d", n, nodeFromLast.data);
 		}
+
 		n = 5;
-		nodeFromLast = FindNthNodeFromLast.findNthNodeFromLast(head,n);
-		if(null != nodeFromLast) {
-			System.out.printf("\nNode from last for n = %d is %d",n,nodeFromLast.data);
+		nodeFromLast = FindNthNodeFromLast.findNthNodeFromLast(head, n);
+		if (null != nodeFromLast) {
+			System.out.printf("\n4. Node from last for n = %d is %d", n, nodeFromLast.data);
 		}
-    }
+	}
 }
--- FindnNodeFromLastInLinkedList/src/main/java/org/learn/Question/FindNthNodeFromLast.java
+++ FindnNodeFromLastInLinkedList/src/main/java/org/learn/Question/FindNthNodeFromLast.java
@@ -3,40 +3,40 @@ package org.learn.Question;
 import org.learn.List.Node;
 
 public class FindNthNodeFromLast {
-	public static Node findNthNodeFromLast(Node head, int n) {		
-		if( null == head) {
-			System.out.println("Linked list is empty");
+	public static Node findNthNodeFromLast(Node head, int n) {
+		if (null == head) {
+			System.out.println("Single linked list is empty");
 			return null;
-		}		
-		
+		}
+
 		Node slow = head;
 		Node fast = head;
 		int index = 0;
-		while(index++ < n ) {
-			if(null == fast) {
-				System.out.printf("\nn=%d is larger than length of linked list",n);
+		while (index++ < n) {
+			if (null == fast) {
+				System.out.printf("\nn=%d is larger than length of linked list", n);
 				return null;
 			}
 			fast = fast.next;
 		}
-		while(fast != null) {
+		while (fast != null) {
 			slow = slow.next;
 			fast = fast.next;
 		}
 		return slow;
 	}
-	
+
 	public static void insert(Node head, int data) {
-		while(head.next != null) 
+		while (head.next != null)
 			head = head.next;
 		head.next = new Node(data);
 	}
-	
+
 	public static void print(Node head) {
-		while(head != null) {
-			System.out.printf("%d ",head.data);
+		while (head != null) {
+			System.out.printf("%d ", head.data);
 			head = head.next;
 		}
-		System.out.println("");		
+		System.out.println("");
 	}
 }
